519b9dd6581972a8e0abf3e7941d39a5780c3041
thanhtoantnt <thanhtoantnt@gmail.com>
2022-08-10 16:53:02 +0800

    add example of abstract contract

--- examples/test_v0.4.sol
+++ examples/test_v0.4.sol
@@ -1,293 +1,45 @@
-/**
- *Submitted for verification at Etherscan.io on 2018-05-16
-*/
-
-pragma solidity ^0.4.11;
-
-
+/* verazt/benchmarks/solidity/unified-dataset/CV/2018-10299.sol */
+pragma solidity ^0.4.16;
 
 /**
-
  * @title SafeMath
-
  * @dev Math operations with safety checks that throw on error
-
  */
-
 library SafeMath {
-
   function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-
     uint256 c = a * b;
-
     assert(a == 0 || c / a == b);
-
     return c;
-
   }
-
-
 
   function div(uint256 a, uint256 b) internal constant returns (uint256) {
-
     // assert(b > 0); // Solidity automatically throws when dividing by 0
-
     uint256 c = a / b;
-
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
     return c;
-
   }
-
-
 
   function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-
     assert(b <= a);
-
     return a - b;
-
   }
-
-
 
   function add(uint256 a, uint256 b) internal constant returns (uint256) {
-
     uint256 c = a + b;
-
     assert(c >= a);
-
     return c;
-
   }
-
 }
-
-
 
 /**
-
- * @title Crowdsale
-
- * @dev Crowdsale is a base contract for managing a token crowdsale.
-
- * Crowdsales have a start and end timestamps, where investors can make
-
- * token purchases and the crowdsale will assign them tokens based
-
- * on a token per ETH rate. Funds collected are forwarded to a wallet
-
- * as they arrive.
-
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
  */
-
-contract token { function transfer(address receiver, uint amount){  } }
-
-contract Crowdsale {
-
-  using SafeMath for uint256;
-
-
-
-  // uint256 durationInMinutes;
-
-  // address where funds are collected
-
-  address public wallet;
-
-  // token address
-
-  address addressOfTokenUsedAsReward;
-
-
-
-  token tokenReward;
-
-
-
-
-
-
-
-  // start and end timestamps where investments are allowed (both inclusive)
-
-  uint256 public startTime;
-
-  uint256 public endTime;
-
-  // amount of raised money in wei
-
-  uint256 public weiRaised;
-
-
-
-  /**
-
-   * event for token purchase logging
-
-   * @param purchaser who paid for the tokens
-
-   * @param beneficiary who got the tokens
-
-   * @param value weis paid for purchase
-
-   * @param amount amount of tokens purchased
-
-   */
-
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-
-
-
-  constructor() {
-
-    wallet = 0x667794B184E4494f8592302f098A889087a58728;
-
-    // durationInMinutes = _durationInMinutes;
-
-    addressOfTokenUsedAsReward = 0x82B99C8a12B6Ee50191B9B2a03B9c7AEF663D527;
-
-
-
-
-
-    tokenReward = token(addressOfTokenUsedAsReward);
-
-  }
-
-
-
-  bool started = false;
-
-
-
-  function startSale(uint256 delay){
-
-    if (msg.sender != wallet || started) throw;
-
-    startTime = block.timestamp + delay * 1 minutes;
-
-    endTime = startTime + 30 * 24 * 60 * 1 minutes;
-
-    started = true;
-
-  }
-
-
-
-  // fallback function can be used to buy tokens
-
-  /* function () payable { */
-
-  /*   buyTokens(msg.sender); */
-
-  /* } */
-
-
-
-  // low level token purchase function
-
-  /* function buyTokens(address beneficiary) payable { */
-
-  /*   /\* require(beneficiary != 0x0); *\/ */
-
-  /*   require(validPurchase()); */
-
-
-
-  /*   uint256 weiAmount = msg.value; */
-
-
-
-  /*   // calculate token amount to be sent */
-
-  /*   uint256 tokens = (weiAmount/10**10) * 1300; */
-
-
-
-  /*   if(block.timestamp < startTime + 1*7*24*60* 1 minutes){ */
-
-  /*     tokens += (tokens * 20) / 100; */
-
-  /*   }else if(block.timestamp < startTime + 2*7*24*60* 1 minutes){ */
-
-  /*     tokens += (tokens * 10) / 100; */
-
-  /*   }else{ */
-
-  /*     tokens += (tokens * 5) / 100; */
-
-  /*   } */
-
-
-
-  /*   // update state */
-
-  /*   weiRaised = weiRaised.add(weiAmount); */
-
-
-
-  /*   tokenReward.transfer(beneficiary, tokens); */
-
-  /*   /\* TokenPurchase(msg.sender, beneficiary, weiAmount, tokens); *\/ */
-
-  /*   forwardFunds(); */
-
-  /* } */
-
-
-
-  // send ether to the fund collection wallet
-
-  // override to create custom fund forwarding mechanisms
-
-  /* function forwardFunds() internal { */
-
-  /*   // wallet.transfer(msg.value); */
-
-  /*   if (!wallet.send(msg.value)) { */
-
-  /*     throw; */
-
-  /*   } */
-
-  /* } */
-
-
-
-  // @return true if the transaction can buy tokens
-
-  function validPurchase() internal constant returns (bool) {
-
-      bool withinPeriod = block.timestamp >= startTime && now <= endTime;
-
-    bool nonZeroPurchase = msg.value != 0;
-
-    return withinPeriod && nonZeroPurchase;
-
-  }
-
-
-
-  // @return true if crowdsale event has ended
-
-  function hasEnded() public constant returns (bool) {
-
-    return block.timestamp > endTime;
-
-  }
-
-
-
-  function withdrawTokens(uint256 _amount) {
-
-    if(msg.sender!=wallet) throw;
-
-    tokenReward.transfer(wallet,_amount);
-
-  }
-
+abstract contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) virtual public constant returns (uint256);
+  function transfer(address to, uint256 value) virtual public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
 }
+
